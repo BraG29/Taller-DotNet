db6e4ba8618c22a0421a2a4931cdc5caedcf3b74
BraG29 <braianbarrios_29@hotmail.com>
2024-10-08 14:57:41 -0300

    DOTNET-4 unificados API-Gateway y Monitor-Client bajo una misma solucion

--- API-Gateway/Program.cs
+++ API-Gateway/Program.cs
@@ -1,28 +1,32 @@
+using k8s.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = DistributedApplication.CreateBuilder(args);
 
+var monitorClient = builder.AddProject<Projects.Monitor_Client>("monitor-client");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// var monitrClient = builder.AddProject<Projects.Monitor_Client>("monitor-client");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+// var app = builder.Build();
+//
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+//
+// app.UseHttpsRedirection();
+//
+// app.UseAuthorization();
+//
+// app.MapControllers();
+//
+// app.Run();
