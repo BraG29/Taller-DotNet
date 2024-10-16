df3f62f1953c7d3e0eda0fd571dce3d433f429aa
ShodytheWolf <martin.antunez5@gmail.com>
2024-10-16 16:06:40 -0300

    Added page files

---
+++ Monitor-Client/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace Monitor_Client.Models
+{
+    public class Client{
+        public Client()
+        {
+
+        }
+        public Client(int CI, int Ap){
+            this.Ci = CI;
+            this.AtenttionPlace = Ap;
+        }
+
+        public int Ci { get; set; }
+        public int AtenttionPlace {  get; set; }
+    }
+}
