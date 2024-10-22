83336b8e9aeca0cb24471002b183b292c847809e
BraG29 <braianbarrios_29@hotmail.com>
2024-10-22 12:07:11 -0300

    DOTNET-6 agregado el Quality-Management al aspire

--- API-Gateway/Program.cs
+++ API-Gateway/Program.cs
@@ -6,7 +6,13 @@ using Microsoft.Identity.Web;
 var builder = DistributedApplication.CreateBuilder(args);
 
 // var monitorClient = builder.AddProject<Projects.Monitor_Client>("monitor-client");
-var comercialOffice = builder.AddProject<Projects.Commercial_Office>("commercial-office");
+
+var comercialOffice = builder
+    .AddProject<Projects.Commercial_Office>("commercial-office");
+
+var qualityManagement = builder
+    .AddProject<Projects.Quality_Management>("quality-management");
+
 
 var app = builder.Build();
 
