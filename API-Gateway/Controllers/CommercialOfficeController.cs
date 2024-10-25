7d25397dc31e0de4e683bd4f4373f48331fa4c55
BraG29 <braianbarrios_29@hotmail.com>
2024-10-25 16:06:00 -0300

    DOTNET-4 API-Gateway ya no funciona como App-Host de aspire. Ejemplos de conexion entre serivcios de Aspire. ServiceDefaults (libreria personalizada con funcionalidades de Aspire)

---
+++ API-Gateway/Controllers/CommercialOfficeController.cs
@@ -0,0 +1,15 @@
+﻿using API_Gateway.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Gateway.Controllers;
+
+public class CommercialOfficeController(CommercialOfficeService officeService) : Controller
+{
+
+    [HttpGet]
+    [Route("/hola")]
+    public string Example()
+    {
+        return officeService.Hi();
+    }
+}
---
+++ API-Gateway/Services/CommercialOfficeService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Gateway.Services;
+
+public class CommercialOfficeService(HttpClient httpClient)
+{
+    public string Hi()
+    {
+        Console.WriteLine("Llamando enpoint en commercial office");
+        var response = httpClient
+            .PutAsJsonAsync("office/releasePosition?officeId=OFI-1&placeNumber=1", new MultipartContent());
+        return response.Result.Content.ReadAsStringAsync().Result;
+        // return "Hola Mundo";
+    }
+    
+}
