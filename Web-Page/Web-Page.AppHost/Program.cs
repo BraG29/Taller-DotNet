ca21c698ee80873af8be87ba0758b0a54f8a83dc
BraG29 <braianbarrios_29@hotmail.com>
2024-10-26 15:55:36 -0300

    Eliminado Web-Page

--- Web-Page/Web-Page.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.Web_Page_ApiService>("apiservice");
-
-builder.AddProject<Projects.Web_Page_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
-builder.Build().Run();
