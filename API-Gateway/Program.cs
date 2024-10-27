a0e2dfbad5abf82ae07f27428141e5298e82ab42
BraG29 <braianbarrios_29@hotmail.com>
2024-10-27 19:28:13 -0300

    Quality-Management reconvertido para ser aplicacion WEB sin Blazor

--- API-Gateway/Program.cs
+++ API-Gateway/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<CommercialOfficeService>(static client =>
 {
+    /*
+     * El nombre usado para el servicio fue el que previamente le configuramos en la app host
+     */
     client.BaseAddress = new("http://commercial-office");
 });
 
