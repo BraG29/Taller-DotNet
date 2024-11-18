4190def882d7c90224c7e67e4a20da585fa6d376
ShodytheWolf <martin.antunez5@gmail.com>
2024-11-18 20:56:10 -0300

    fix, to the fix to the merge

---
+++ Commercial-Office/Migrations/20241118232827_Create Commercial_Office_DB table .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Commercial_Office.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCommercial_Office_DBtable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
