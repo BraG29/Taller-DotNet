0de64593a8d80f0694d79b87819a7a42dab7014e
BraG29 <braianbarrios_29@hotmail.com>
2024-11-21 10:14:54 -0300

    DOTNET-8 servicio de autenticacion implementado

---
+++ Authentication/DataAccess/AuthenticationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Authentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.DataAccess;
+
+public class AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(e => e.Username)
+            .IsUnique();
+    }
+}
--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,11 +1,34 @@
+using Authentication.DataAccess;
+using Authentication.Models;
+using Authentication.Repositories;
+using Authentication.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<JwtTokenService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("AuthDatabase");
+
+builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policyBuilder =>
+    {
+        policyBuilder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -16,9 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
-app.UseAuthorization();
+// app.UseHttpsRedirection();
+//
+// app.UseAuthorization();
 
 app.MapControllers();
 
